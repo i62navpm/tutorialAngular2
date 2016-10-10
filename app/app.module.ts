a209095443f5c311116826b55671a4d14bdf9bd5
Manuel Navarro <manuelnavarro1987@gmail.com>
2016-10-10 23:40:37 +0200

    7.HTTP

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,19 +1,28 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
+import { HttpModule }     from '@angular/http';
 import { RouterModule }   from '@angular/router';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
 import { DashboardComponent }  from './dashboard.component';
 import { HeroService }         from './hero.service';
+import { HeroSearchComponent } from './hero-search.component'
 
-
+import './rxjs-extensions';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     RouterModule.forRoot([
       {
         path: '',
@@ -38,7 +47,8 @@ import { HeroService }         from './hero.service';
     AppComponent,
     HeroDetailComponent,
     HeroesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   providers: [
     HeroService
